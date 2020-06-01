e3e4c87d9b839eb82d51c87e4c659d3d5ac371da
dev-to-dev <gashe7@gmail.com>
2020-06-01 19:44:48 +0900

    200601

--- 200601/level2_타겟-넘버.cpp
+++ 200601/level2_타겟-넘버.cpp
@@ -7,14 +7,17 @@ using namespace std;
 
 int answer;
 
+// dfs 재귀 이용
+// vector, target
+// idx : 현재 인덱스 val: 현재 값
 void dfs(vector<int> num, int target, int idx, int val){
-    
+    //만약 모든 경우 확인
     if(idx == num.size()){
-        if(val == target) answer++;
+        if(val == target) answer++;	// 현재 값이 target과 같다면
         return;
     }
-    dfs(num, target, idx+1, val + num[idx]);
-    dfs(num, target, idx+1, val - num[idx]);
+    dfs(num, target, idx+1, val + num[idx]);	// + 경우
+    dfs(num, target, idx+1, val - num[idx]);	// - 경우
     return;    
 }
 
