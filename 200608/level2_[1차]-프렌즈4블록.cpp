4cdb7eb3a7cecdc461006a03e9e97bf8e804b72b
dev-to-dev <gashe7@gmail.com>
2020-06-09 01:20:53 +0900

    solve 프렌즈4블록

--- 200608/level2_[1차]-프렌즈4블록.cpp
+++ 200608/level2_[1차]-프렌즈4블록.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 int board[30][30];
 bool visit[30][30];
-int answer, col, row;
+int col, row;
 
 int dx[3] ={0,1,1};
 int dy[3] ={1,0,1};
@@ -26,16 +26,16 @@ bool isBlock(int x, int y){
     for(int i =0;i<3;i++){
         int nx = x + dx[i];
         int ny = y + dy[i];
-        if(nx < 0 || nx > row || ny < 0 || ny > col || board[nx][ny] != board[x][y])
+        // 문제점 찾아냄. nx > row => nx >= row 
+        if(nx < 0 || nx >= row || ny < 0 || ny >= col || board[nx][ny] != board[x][y])
             return false;
     }
     return true;
 }
 
-
 // 2.
 void bfs(int x,int y){
-    if(!isBlock(x,y))
+    if(visit[x][y] || !isBlock(x,y))
         return;
     
     queue<pair<int,int>> q;
@@ -59,31 +59,26 @@ void bfs(int x,int y){
     }
 }
 
-
 // 3. 
 int check(){
     int ret = 0;
     for(int i =0;i<row;i++){
         for(int j =0;j<col;j++){
             if(visit[i][j]){
-                answer++;
                 board[i][j] = -1;
                 ret++;
             }
         }
     }
-    memset(visit,0,sizeof(visit));
     return ret;
 }
 
-//3.
 void fill(){
     for(int i=0; i<col;i++){
         for(int j = row-1 ; j>=0;j--){
-            
             if(board[j][i] != -1)
                 continue;
-            int idx = j-1;
+            int idx = j;
             while(board[idx][i] == -1 && idx>=0){idx--;}
             
             if(idx >=0){
@@ -93,24 +88,23 @@ void fill(){
             else
                 break;
         }
-        
     }
     return;
 }
 
 
 int solution(int m, int n, vector<string> b) {
-    answer = 0;
+    int answer = 0;
     row = m, col =n;
     
-    for(int i =0;i<b.size();i++){
-        for(int j=0;j<b[i].length();j++){
+    for(int i =0;i<m;i++){
+        for(int j=0;j<n;j++){
             board[i][j] = b[i][j]-'A';
         }
     }
     
-    // 반복
     while(true){
+        memset(visit,0,sizeof(visit));
         for(int i=0;i<row-1;i++){
             for(int j= 0 ;j<col-1;j++){
                 if(board[i][j]!=-1)
@@ -118,7 +112,8 @@ int solution(int m, int n, vector<string> b) {
             }
         }
         int ret = check();
-        if(ret == 0)
+        answer += ret;
+        if(!ret)
             break;
         fill();
     }
