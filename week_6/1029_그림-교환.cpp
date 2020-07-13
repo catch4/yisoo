3e45c33936744a0ec02d1e3fba2f79c48a88787e
dev-to-dev <gashe7@gmail.com>
2020-07-13 15:59:04 +0900

    fail

---
+++ week_6/1029_그림-교환.cpp
@@ -0,0 +1,44 @@
+// 백준 1029번 그림 교환
+
+//
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int n;
+int p[15][15];
+int dp[1 << 15][15][10];
+
+int solve(int here, int visit, int price) {
+	int& ret = dp[visit][here][price];
+	if (ret != -1) return ret;
+	ret = 1;
+	for (int i = 0; i < n; i++) {
+		if ((1 << i) & visit) continue;
+		if (p[here][i] >= price)
+			ret = max(ret, 1 + solve(i, visit | (1 << i), p[here][i]));
+	}
+	return ret;
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		string tmp;
+		cin >> tmp;
+		for (int j = 0; j < n; j++) {
+			p[i][j] = tmp[j] - '0';
+		}
+	}
+	memset(dp, -1, sizeof(dp));
+
+	cout << solve(0, 1, 0);
+	return 0;
+}
