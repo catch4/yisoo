9f851885a24b50a3f80263382e8ab7da23562c72
dev-to-dev <gashe7@gmail.com>
2020-09-28 23:55:26 +0900

    solve BOJ1647 Kruskal

--- week_16/1647_도시-분할-계획.cpp
+++ week_16/1647_도시-분할-계획.cpp
@@ -2,7 +2,7 @@
 
 /*
 MST 만든 후 최대값 삭제
-프림 알고리즘 사용
+크루스칼 알고리즘 사용
 */
 
 #include <algorithm>
@@ -12,55 +12,61 @@ MST 만든 후 최대값 삭제
 
 using namespace std;
 
-typedef pair<int, int> pi;
+typedef struct Node {
+    int c, a, b;
+} node;
 
 int n, m;
-vector<pi> adj[100001];
-bool v[100001];
+vector<node> adj;
+
+int p[100001];
+
+bool cmp(node x, node y)
+{
+    return x.c < y.c;
+}
+int find(int idx)
+{
+    if (p[idx] == idx)
+        return idx;
+    return p[idx] = find(p[idx]);
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+
     cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        adj[a].push_back({ c, b });
-        adj[b].push_back({ c, a });
+
+    for (int i = 1; i <= n; i++) {
+        p[i] = i;
     }
+
+    for (int i = 0; i < m; i++) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        adj.push_back({ z, x, y });
+    }
+
+    sort(adj.begin(), adj.end(), cmp);
 
     int sum = 0;
-    int maxVal = 0;
+    int cnt = 0;
 
-    priority_queue<pi, vector<pi>, greater<pi>> pq;
-    int start;
-    for (int i = 0; i < n; i++) {
-        if (adj[i].size() != 0) {
-            start = i;
-            break;
-        }
-    }
-    v[start] = 1;
-    int cnt = 1;
-    for (int i = 0; i < adj[start].size(); i++) {
-        pq.push(adj[start][i]);
-    }
-    while (!pq.empty()) {
-        int cost = pq.top().first;
-        int next = pq.top().second;
-        pq.pop();
-        if (v[next])
+    for (node arr : adj) {
+        int c = arr.c, x = arr.a, y = arr.b;
+
+        int i = find(x), j = find(y);
+        if (i == j)
             continue;
-        v[next] = 1;
-        sum += cost;
-        maxVal = max(maxVal, cost);
-        if (++cnt == n)
+        p[j] = i;
+        sum += c;
+        if (++cnt == n - 2)
             break;
-        for (int i = 0; i < adj[next].size(); i++) {
-            pq.push(adj[next][i]);
-        }
     }
-    cout << sum - maxVal;
+    cout << sum;
+
     return 0;
 }
