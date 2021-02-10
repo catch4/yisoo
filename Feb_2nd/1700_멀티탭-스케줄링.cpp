b0ac7ace04cd619db854572b14d9d6348574dc4c
dev-to-dev <gashe7@gmail.com>
2021-02-10 21:35:36 +0900

    주석 변경

--- Feb_2nd/1700_멀티탭-스케줄링.cpp
+++ Feb_2nd/1700_멀티탭-스케줄링.cpp
@@ -1,10 +1,7 @@
 // 백준 1700번 멀티탭-스케줄링
 
 /*
-1. 모든 숫자의 빈도수 계산
-2. 칸마다 제일 적은 빈도수를 빼기.
-
-
+1. 바꿔야할 때 뒤에서 제일 늦게 나오는 수를 대체
 */
 
 #include <algorithm>
@@ -20,8 +17,10 @@ vector<int> plug;
 unordered_map<int, vector<int>> m;
 bool check[101];
 
+// 플러그 꽂힌 수의 제일 최근에 나오는 index 찾아내기.
 int getIdx(int idx, int val)
 {
+    // val보다 큰 인덱스 중에 가장 가까운 수 골라내기 위해 lowerbound 사용
     auto ret = lower_bound(m[plug[idx]].begin(), m[plug[idx]].end(), val) - m[plug[idx]].begin();
 
     if (ret == m[plug[idx]].size())
@@ -38,7 +37,7 @@ int main()
     cin >> n >> k;
     for (int i = 0; i < k; i++) {
         cin >> arr[i];
-        m[arr[i]].push_back(i);
+        m[arr[i]].push_back(i); // 해당 숫자가 나오는 index 저장
     }
 
     if (n >= k) {
@@ -52,15 +51,18 @@ int main()
     for (int i = 0; i < k; i++) {
         if (check[arr[i]])
             continue;
+
+        // 멀티탭 비었으면
         if (empty != 0) {
 
             plug.push_back(arr[i]);
             check[arr[i]] = 1;
             empty--;
-        } else {
+        } else { // 플러그 뽑아야 할 때
 
             int val = -1;
             int minIdx = -1;
+            // index가 제일 높은 수 골라내기
             for (int idx = 0; idx < n; idx++) {
                 int tmp = getIdx(idx, i);
                 if (tmp > val) {
@@ -68,7 +70,7 @@ int main()
                     minIdx = idx;
                 }
             }
-            //cout << i << " " << plug[minIdx] << " " << arr[i] << endl;
+            // 고른 숫자 대체하기
             check[plug[minIdx]] = 0;
             check[arr[i]] = 1;
 
